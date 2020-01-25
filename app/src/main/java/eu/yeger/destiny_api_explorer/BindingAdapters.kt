65c9453169ee16adacfdcf5311e5187487f0d8c2
Jan Müller <janmueller3698@gmail.com>
2020-01-25 13:31:59 +0100

    Improved binding adapters

--- app/src/main/java/eu/yeger/destiny_api_explorer/BindingAdapters.kt
+++ app/src/main/java/eu/yeger/destiny_api_explorer/BindingAdapters.kt
@@ -11,29 +11,28 @@ import eu.yeger.destiny_api_explorer.network.BUNGIE_BASE_URL
 import eu.yeger.destiny_api_explorer.ui.ItemGridAdapter
 
 @BindingAdapter("imageUrl")
-fun bindImage(imgView: ImageView, imageUrl: String?) {
+fun ImageView.bindImage(imageUrl: String?) {
     imageUrl?.let {
         val imgUri = imageUrl.toUri().buildUpon().scheme("https").build()
-        Glide.with(imgView.context)
+        Glide.with(context)
             .load(imgUri)
-            .into(imgView)
+            .into(this)
     }
 }
 
 @BindingAdapter("bungieImageUrl")
-fun bindBungieImage(imageView: ImageView, imageUrl: String?) =
-    bindImage(imageView, BUNGIE_BASE_URL + imageUrl)
+fun ImageView.bindBungieImage(imageUrl: String?) =
+    bindImage(BUNGIE_BASE_URL + imageUrl)
 
 @BindingAdapter("itemList")
-fun bindRecyclerView(recyclerView: RecyclerView, data: List<ItemDefinition>?) {
-    val adapter = recyclerView.adapter as ItemGridAdapter
+fun RecyclerView.bindRecyclerView(data: List<ItemDefinition>?) {
+    val adapter = adapter as ItemGridAdapter
     adapter.submitList(data)
 }
 
 @BindingAdapter("onRefresh")
-fun bindSwipeRefreshLayout(swipeRefreshLayout: SwipeRefreshLayout, listener: Runnable) {
-    swipeRefreshLayout.setOnRefreshListener {
+fun SwipeRefreshLayout.bindSwipeRefreshLayout(listener: Runnable) {
+    setOnRefreshListener {
         listener.run()
     }
 }
-
